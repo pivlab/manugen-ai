1592ece816fc9cf772e486b9e29765fb1e726ddc
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2025-06-10 10:03:39 -0600

    Adds postgres, integrates as session db for backend's adk web instance

--- backend/start_adk_web.sh
+++ backend/start_adk_web.sh
@@ -2,5 +2,11 @@
 
 cd /opt/manugen-ai/src/
 
+if [ -z "${SESSION_DB_CONN_STRING}" ]; then
+    SESSION_DB_ARG=""
+else
+    SESSION_DB_ARG="--session_db_url ${SESSION_DB_CONN_STRING}"
+fi
+
 uv run --project /app \
-     adk web --host 0.0.0.0 --port 8000
+     adk web --host 0.0.0.0 --port 8000 ${SESSION_DB_ARG}
