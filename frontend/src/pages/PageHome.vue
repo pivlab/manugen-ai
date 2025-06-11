6a97a948a562ae073cb8066bfbe5a47c88152ea4
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-06-11 10:15:11 -0400

    scaffold

---
+++ frontend/src/pages/PageHome.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="text-emerald-800 p-8">Hello World</div>
+</template>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+import PageHome from "../pages/PageHome.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: PageHome,
+    },
+  ],
+});
+
+export default router;
