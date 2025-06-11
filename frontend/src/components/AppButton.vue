79a859b19fe209687924cab7a8be4c51c8306ece
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-06-11 13:14:50 -0400

    upload download

---
+++ frontend/src/components/AppButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="flex justify-center items-center p-2 min-w-10 min-h-10 bg-indigo-500 text-white rounded-full cursor-pointer hover:bg-indigo-400 transition-all"
+  >
+    <slot />
+  </button>
+</template>
