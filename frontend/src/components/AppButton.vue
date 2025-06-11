72225f72c4cc0502769f6022f85630d6e777fefb
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-06-11 14:51:13 -0400

    agent badges

--- frontend/src/components/AppButton.vue
+++ frontend/src/components/AppButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="flex justify-center items-center p-2 min-w-10 min-h-10 bg-indigo-500 text-white rounded-full cursor-pointer hover:bg-indigo-400 transition-all"
+    class="flex justify-center items-center min-w-10 min-h-10 bg-indigo-500 text-white rounded-full cursor-pointer hover:bg-indigo-400 transition-all"
   >
     <slot />
   </button>
