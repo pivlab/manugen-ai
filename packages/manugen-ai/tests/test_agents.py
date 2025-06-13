693ba33f0c8aa62b7ad98128fe1b4314034303e8
d33bs <ekgto445@gmail.com>
2025-06-13 13:18:50 -0600

    add capitalizer agent and adjust test

--- packages/manugen-ai/tests/test_agents.py
+++ packages/manugen-ai/tests/test_agents.py
@@ -2,28 +2,27 @@
 Tests for various agents
 """
 
-from manugen_ai.agents.repo_to_paper.agent import root_agent
+from manugen_ai.agents.capitalizer.agent import root_agent
 from manugen_ai.utils import run_agent_workflow
 import pytest
 
 
 @pytest.mark.asyncio
-async def test_something():
-    APP_NAME = "db_tool_app"
-    USER_ID = "repo_to_paper_user"
-    SESSION_ID = "session_0001"
-    final_output, session_state, output_events = await run_agent_workflow(
+async def test_agent_capitalizer():
+    APP_NAME = "app"
+    USER_ID = "user"
+    SESSION_ID = "001"
+    _, session_state, _ = await run_agent_workflow(
         agent=root_agent,
         prompt="""
-        Here's url for the repository you will turn into an abstract:
-        https://github.com/manubot/manubot-ai-editor
+        this is a sentence to correct
         """,
         app_name=APP_NAME,
         user_id=USER_ID,
         session_id=SESSION_ID,
-        initial_state={"current_document": "stub text"},
         # note: use `uv run pytest -s` to view the verbose stdout
         verbose=True,
     )
     # assert we have an abstract which was added
-    assert "abstract" in session_state.keys()
+    assert "output" in session_state.keys()
+    assert session_state["output"] == "This is a sentence to correct."
