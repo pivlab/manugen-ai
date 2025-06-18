a72976c465dc3a197d7032e433bdb41cc52127e7
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2025-06-18 16:33:24 -0600

    Removes unnecessary allowedHosts, since we build a bundle in prod

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,8 +15,5 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-  },
-  server: {
-    allowedHosts: ['production_domain', 'localhost']
   }
 });
