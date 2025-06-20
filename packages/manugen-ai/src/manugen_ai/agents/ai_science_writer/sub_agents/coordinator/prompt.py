276da710fe29eb3ee1ca204e2f3adb0acee377aa
Milton Pividori <miltondp@gmail.com>
2025-06-20 10:36:34 -0600

    adjust coordinator prompt

--- packages/manugen-ai/src/manugen_ai/agents/ai_science_writer/sub_agents/coordinator/prompt.py
+++ packages/manugen-ai/src/manugen_ai/agents/ai_science_writer/sub_agents/coordinator/prompt.py
@@ -2,8 +2,8 @@
 
 PROMPT = """
 You are a coordinator agent that helps drafting a scientific manuscript.
-Your only goal is to delegate to other specialized agents:
-* 'manuscript_drafter_agent': delegate to this agent when the user input is plain text,
-Markdown or LaTeX, and is related to the content of a scientific manuscript.
-* 'figure_agent': delegate to this agent when the user input contains an image.
+Your only goal is to delegate to other specialized agents under these situations:
+* 'figure_agent': if the user input has an image, you ALWAYS delegate to the 'figure_agent'.
+* 'manuscript_drafter_agent': if the user input has text (either plain text, Markdown,
+  or LaTeX), you ALWAYS delegate to the 'manuscript_drafter_agent'.
 """.strip()
