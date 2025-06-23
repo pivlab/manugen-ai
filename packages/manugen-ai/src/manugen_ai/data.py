62256868e19152521e0f08c63b2328da4d3184d6
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2025-06-23 16:13:31 -0600

    Adds Gemini GenAI API embedding support, enabled by default. Moves bge-m3 model download to backend/start_api_server.sh, only if not using Gemini for embeddings

--- packages/manugen-ai/src/manugen_ai/data.py
+++ packages/manugen-ai/src/manugen_ai/data.py
@@ -2,28 +2,122 @@
 #    !pip install duckdb transformers FlagEmbedding polars
 
 import logging
+import os
 import pathlib
 
 import duckdb
 import numpy as np
 import pyarrow as pa
-import torch
-from FlagEmbedding import BGEM3FlagModel
 
 from manugen_ai.utils import download_file_if_not_available
 
-# avoid warnings from transformers
-logging.getLogger("transformers").setLevel(logging.ERROR)
+# if USE_GEMINI_EMBEDDINGS is 1, we'll use
+USE_GEMINI_EMBEDDINGS = os.environ.get("USE_GEMINI_EMBEDDINGS", "1") == "1"
 
-# set device and model for data work
-DEVICE = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
-    else "cpu"
+# ----------------------------------------------------
+# --- Gemini embeddings setup
+# ----------------------------------------------------
+
+GEMINI_EMBEDDING_MODEL_NAME = os.environ.get(
+    "GEMINI_EMBEDDING_MODEL_NAME", "gemini-embedding-exp-03-07"
 )
-EMBEDDING_MODEL = BGEM3FlagModel("BAAI/bge-m3", use_fp16=True, device=DEVICE)
+
+# singleton for the google genai client
+# set the first time get_genai_client() is called
+_GENAI_CLIENT = None
+
+
+def get_genai_client():
+    """
+    Get the Google GenAI client for generating embeddings.
+
+    This function initializes the GenAI client if it has not been
+    created yet, ensuring that the client is loaded only once and reused
+    across calls.
+
+    Returns:
+        google.genai.client: The initialized GenAI client.
+    """
+    global _GENAI_CLIENT
+
+    if _GENAI_CLIENT is None:
+        from google import genai
+
+        # initialize the GenAI client
+        _GENAI_CLIENT = genai.Client(api_key=os.environ.get("GOOGLE_API_KEY"))
+
+    return _GENAI_CLIENT
+
+
+# ----------------------------------------------------
+# --- FlagEmbedding embeddings setup
+# ----------------------------------------------------
+
+FLAGEMBEDDING_MODEL_OR_PATH = os.environ.get(
+    "FLAGEMBEDDING_MODEL_OR_PATH", "BAAI/bge-m3"
+)
+FLAGEMBEDDING_CACHE_DIR = os.environ.get("FLAGEMBEDDING_CACHE_DIR", "/opt/model_cache/")
+
+# singleton for the flagembedding model
+# set the first time get_flag_embedding_model() is called
+_EMBEDDING_MODEL = None
+
+
+def get_flag_embedding_model():
+    """
+    Get the FlagEmbedding model for generating dense vector embeddings.
+
+    This function initializes the embedding model if it has not been
+    created yet, ensuring that the model is loaded only once and reused
+    across calls.
+
+    Returns:
+        BGEM3FlagModel: The initialized embedding model.
+    """
+    global _EMBEDDING_MODEL
+
+    if _EMBEDDING_MODEL is None:
+        # import these here, as they're only needed when we're using BGE-M3
+        import torch
+        from FlagEmbedding import BGEM3FlagModel
+
+        # avoid warnings from transformers
+        logging.getLogger("transformers").setLevel(logging.ERROR)
+
+        # set device and model for data work
+        device = (
+            "cuda"
+            if torch.cuda.is_available()
+            else "mps"
+            if torch.backends.mps.is_available()
+            else "cpu"
+        )
+
+        _EMBEDDING_MODEL = BGEM3FlagModel(
+            FLAGEMBEDDING_MODEL_OR_PATH,
+            cache_dir=FLAGEMBEDDING_CACHE_DIR,
+            use_fp16=True,
+            device=device,
+        )
+
+    return _EMBEDDING_MODEL
+
+
+# ----------------------------------------------------
+# --- General Withdrarxiv Encoding and Search
+# ----------------------------------------------------
+
+
+def get_model_name() -> str:
+    """
+    Get the name of the embedding model being used.
+    """
+    if USE_GEMINI_EMBEDDINGS:
+        # use the gemini model name
+        return GEMINI_EMBEDDING_MODEL_NAME
+    else:
+        # Extract model name from path
+        return FLAGEMBEDDING_MODEL_OR_PATH.split("/")[-1]
 
 
 def embed(text: str) -> np.ndarray:
@@ -43,8 +137,64 @@ def embed(text: str) -> np.ndarray:
         >>> vec.shape
         (768,)
     """
-    vec = EMBEDDING_MODEL.encode([text], batch_size=4)["dense_vecs"][0]
+    if USE_GEMINI_EMBEDDINGS:
+        embeddings = (
+            get_genai_client()
+            .models.embed_content(model=GEMINI_EMBEDDING_MODEL_NAME, contents=[text])
+            .embeddings[0]
+            .values
+        )
+
+        vec = np.array(embeddings, dtype=np.float32)
+    else:
+        vec = get_flag_embedding_model().encode([text], batch_size=4)["dense_vecs"][0]
+
     return vec.astype(np.float32)
+
+
+def embed_batch(texts: list[str]) -> np.ndarray:
+    """
+    Generate dense vector embeddings for a batch of texts.
+
+    Args:
+        texts (list[str]):
+          A list of input texts to embed.
+
+    Returns:
+        np.ndarray:
+          A NumPy array of shape (n, 1024) containing the dense vector representations
+          of the input texts, where n is the number of texts.
+
+    Example:
+        >>> vecs = embed_batch(["Hello world", "Another text"])
+        >>> vecs.shape
+        (2, 1024)
+    """
+    if USE_GEMINI_EMBEDDINGS:
+        embeddings = [
+            x.values
+            for x in get_genai_client()
+            .models.embed_content(model=GEMINI_EMBEDDING_MODEL_NAME, contents=texts)
+            .embeddings
+        ]
+        embs = np.array(embeddings, dtype=np.float32)
+    else:
+        embs = get_flag_embedding_model().encode(texts, batch_size=4)["dense_vecs"]
+
+    return embs.astype(np.float32)
+
+
+def get_embedding_size() -> int:
+    """
+    Get the size of the dense vector embeddings.
+
+    BAAI/bge-m3 embeddings have 1024 components ,
+    while Gemini embeddings have 768.
+
+    Returns:
+        int: The size of the dense vector embeddings,
+    """
+    return 768 if USE_GEMINI_EMBEDDINGS else 1024
 
 
 def create_withdrarxiv_embeddings(
@@ -83,6 +233,10 @@ def create_withdrarxiv_embeddings(
         >>> db_path = create_withdrarxiv_embeddings()
         >>> print(f"Embeddings stored in: {db_path}")
     """
+
+    if target_db is None:
+        target_db = f"withdrarxiv_embeddings_{get_model_name()}.duckdb"
+
     # Connect to (or create) your DuckDB database
     conn = duckdb.connect(str(pathlib.Path(__file__).parent / "data" / target_db))
 
@@ -112,15 +266,17 @@ def create_withdrarxiv_embeddings(
     )
 
     conn.execute(
-        """
+        f"""
         CREATE OR REPLACE TABLE embeddings (
           arxiv_id VARCHAR,
-          embedding FLOAT[1024]
+          embedding FLOAT[{get_embedding_size()}]
         );
         """
     )
 
-    conn.create_function("embed", embed, [duckdb.typing.VARCHAR], "FLOAT[1024]")
+    conn.create_function(
+        "embed", embed, [duckdb.typing.VARCHAR], f"FLOAT[{get_embedding_size()}]"
+    )
 
     # Batch-compute embeddings for every abstract
     batch_size = 100
@@ -139,9 +295,7 @@ def create_withdrarxiv_embeddings(
             break
 
         texts = batch_table["abstract"].to_pylist()
-        embs = EMBEDDING_MODEL.encode(texts, batch_size=4)["dense_vecs"].astype(
-            np.float32
-        )
+        embs = embed_batch(texts)
 
         embedding_column = pa.array(embs.tolist(), type=pa.list_(pa.float32()))
         batch_with_emb = batch_table.append_column("embedding", embedding_column)
@@ -187,24 +341,40 @@ def search_withdrarxiv_embeddings(query: str, top_k: int = 2):
         >>> print(df.head())
     """
 
-    target_db = str(
-        pathlib.Path(__file__).parent / "data" / "withdrarxiv_embeddings.duckdb"
-    )
+    # retrieve a precomputed embeddings database based on the model we're using
+    model_name = get_model_name()
 
-    # ensure we have the file available by using a remote backup
-    target_db = download_file_if_not_available(
-        local_path=target_db,
-        download_url=(
+    if model_name == "text-embedding-004":
+        src_url = "https://olucdenver-my.sharepoint.com/:u:/g/personal/dave_bunten_cuanschutz_edu/EUxMDRvp-zZInkE5NqNYz0ABIkKtOO-5DFIrkBeVbi_p_A?download=1"
+    elif model_name == "bge-m3":
+        src_url = (
             "https://olucdenver-my.sharepoint.com/:u:"
             "/g/personal/dav"
             "e_bunten_cuansc"
             "hutz_edu/EdC8fHAME2dLm5G9A"
             "xtVbL0B7EAf4cWF3XCXwZTPYNWa2A?download=1"
-        ),
+        )
+    else:
+        raise Exception(
+            f"No embeddings file exists for model {model_name}, please run the embedding creation first."
+        )
+
+    # construct path to the DuckDB database
+    target_db_path = str(
+        pathlib.Path(__file__).parent
+        / "data"
+        / f"withdrarxiv_embeddings_{model_name}.duckdb"
+    )
+
+    target_db = download_file_if_not_available(
+        local_path=target_db_path,
+        download_url=src_url,
     )
 
     conn = duckdb.connect(target_db)
-    conn.create_function("embed", embed, [duckdb.typing.VARCHAR], "FLOAT[1024]")
+    conn.create_function(
+        "embed", embed, [duckdb.typing.VARCHAR], f"FLOAT[{get_embedding_size()}]"
+    )
 
     q = {"q": query, "k": top_k}
     df = (
